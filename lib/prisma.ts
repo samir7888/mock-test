f427ab7946d25b2357354ae7679b7d70b3fc7252
samir7888 <basnetsameer78@gmail.com>
2026-05-24 11:28:48 +0545

    fixed primsaa

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from '@/app/generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
-function createPrismaClient() { 
+function createPrismaClient() {
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
-  })
-  return new PrismaClient({ adapter })
+  });
+  return new PrismaClient({ adapter });
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
